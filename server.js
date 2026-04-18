a508d9e9390b369666aeac6755715ae8026fc823
pari2611 <pari.gupta1@s.amity.edu>
2026-04-19 02:47:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
+app.options('*', cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
